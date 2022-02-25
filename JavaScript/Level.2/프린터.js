427ec7f4b53a13b6e9d47cd9bd98febdff4665e7
Sangho <istrangeho@gmail.com>
2022-02-25 23:38:12 +0900

    Queue & Stack

--- JavaScript/Level.2/프린터.js
+++ JavaScript/Level.2/프린터.js
@@ -1,12 +1,10 @@
 function solution(priorities, location) {
-    let progress = 0;
     var answer = 0;
     while (true) {
         let priority = priorities[0];
         let flag = true;
         for (let i = 0; i < priorities.length; ++i) {
             if (priorities[i] > priority) {
-                console.log(i, priority);
                 priorities.push(priority);
                 priorities.shift();
                 if (location === 0) {
