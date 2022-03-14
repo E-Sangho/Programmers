030f30117549bb584f2b76946a622f68eb33741a
Sangho <istrangeho@gmail.com>
2022-03-14 23:32:25 +0900

    2020 Kakao Internship

--- C++/etc/combination.cpp
+++ C++/etc/combination.cpp
@@ -61,6 +61,23 @@ void combination2(string input, vector<char> arr, vector<string> &answer, int in
 
 }
 
+void combination3(string input, vector<char> arr, vector<string> &answer, int start, int index) {
+    if(index == 3) {
+        string s = "";
+        for(int i = 0; i < arr.size(); ++i) {
+            s +=  arr[i];
+        }
+        answer.push_back(s);
+        return;
+    }
+
+    for(int i = start; i < input.size() && input.size() - i >= 3 - index; ++i) {
+        arr[index] = input[i];
+        combination2(input, arr, answer, i+1, index+1);
+    }
+
+}
+
 void print(vector<string> v) {
     for(int i = 0; i < v.size(); ++i) {
         cout << v[i] << endl;
@@ -72,6 +89,6 @@ int main() {
     vector<string> answer;
     int r = 3;
     vector<char> arr(r);
-    combination2(input, arr, answer, 0, 0);
+    combination3(input, arr, answer, 0, 0);
     print(answer);
 }
